089ed0fbd53fcbe0999c4431e9d21a82ca86df3c
Richard Munoz <richosojason@msn.com>
2019-09-16 12:42:27 -0500

    added receive message

---
+++ messageManager/sqs_receivemessage.js
@@ -0,0 +1,44 @@
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({ region: 'us-east-2' });
+
+// Create an SQS service object
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+
+const queueURL = "https://sqs.us-east-2.amazonaws.com/817439561489/SQS_QUEUE_NODE";
+
+const params = {
+  AttributeNames: [
+    "SentTimestamp"
+  ],
+  MaxNumberOfMessages: 10,
+  MessageAttributeNames: [
+    "All"
+  ],
+  QueueUrl: queueURL,
+  VisibilityTimeout: 20,
+  WaitTimeSeconds: 0
+};
+
+sqs.receiveMessage(params, (err, data) => {
+  if (err) {
+    console.log("Receive Error", err);
+  } else if (data.Messages) {
+    const deleteParams = {
+      QueueUrl: queueURL,
+      ReceiptHandle: data.Messages[0].ReceiptHandle
+    };
+    console.log("Messages", data.Messages);
+
+    console.log("Receipt handle:", data.Messages[0].ReceiptHandle);
+
+    sqs.deleteMessage(deleteParams, (err, data) => {
+      if (err) {
+        console.log("Delete Error", err);
+      } else {
+        console.log("Message Deleted", data);
+      }
+    });
+  }
+});

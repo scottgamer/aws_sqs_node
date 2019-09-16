a55851c2eb812fdc31301d0a34ed6157c0d36e26
Richard Munoz <richosojason@msn.com>
2019-09-16 10:59:41 -0500

    added list queues

---
+++ queueManager/sqs_listqueues.js
@@ -0,0 +1,22 @@
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({
+  region: 'us-east-2'
+});
+// Set credentials
+const credentials = new AWS.SharedIniFileCredentials({profile: 'sqs-profile'});
+AWS.config.credentials = credentials;
+
+// Create an SQS service object
+const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+
+const params = {};
+
+sqs.listQueues(params, (err, data) => {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.QueueUrls);
+  }
+});
